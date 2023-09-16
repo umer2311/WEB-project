219f6e916bedc837a518cc3976aa5d9337fb3c1e
umer2311 <umernawaz2000@gmail.com>
2023-09-16 02:19:17 -0700

    firstCommit

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log(23);
+console.log("hello world");
+console.log(200);
+console.log(3.5+23);
+console.log(2023-1969);
+console.log(65/240);
+console.log(0.2708*100);
+console.log('Hello'+'World');
+console.log('Hello'+' '+'World');
